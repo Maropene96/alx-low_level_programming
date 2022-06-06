a34ec602f521307078dc664f4680a987ebc5be01
Maropene <rashetlemaropene@gmail.com>
2022-06-06 08:30:46 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * main - Prints the alphabet without q and e
+ * main - Prints the alphabet without q and e.
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,14 +11,11 @@ int main(void)
 
 	for (i = 97; i < 123; i++)
 	{
-		if (i == 100 || i == 112)
+		if (i != 101 && i != 113)
 		{
 			putchar(i);
-			i++;
-		else
-			puchar(i);
 		}
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
